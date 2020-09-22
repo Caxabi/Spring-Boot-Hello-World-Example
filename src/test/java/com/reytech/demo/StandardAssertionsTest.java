3e8ae8e184fde389c7133d48ea22bd52548903c2
Caxabi <58560867+Caxabi@users.noreply.github.com>
2020-09-22 11:46:11 +0200

    Update StandardAssertionsTest.java
    Test fail Nassure

--- src/test/java/com/reytech/demo/StandardAssertionsTest.java
+++ src/test/java/com/reytech/demo/StandardAssertionsTest.java
@@ -30,7 +30,7 @@ class StandardAssertionsTest {
     
     @Test
     void groupedAssertions() {
-        Personne pers = new Personne("Nassur", "Moumadi");
+        Personne pers = new Personne("Nassure", "Moumadi");
 
         assertAll("personne", () -> assertEquals("Nassur", pers.getFirstName()),
                               () -> assertEquals("Moumadi", pers.getLastName()));
